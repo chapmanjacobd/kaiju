c4d25be7f81b8bc8c56de8a1d6f8ed1537afa0a8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2021-07-04 15:08:11 -0500

    wip

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-import Alpine from 'alpinejs';
-import hepburn from 'hepburn';
-import heads from 'url:./h/*.jpg';
-import bodies from 'url:./b/*.jpg';
-import tails from 'url:./t/*.jpg';
+import Alpine from "alpinejs";
+import hepburn from "hepburn";
+import heads from "url:./h/*.jpg";
+import bodies from "url:./b/*.jpg";
+import tails from "url:./t/*.jpg";
 
 window.Alpine = Alpine;
 
-document.addEventListener('alpine:init', () => {
-    Alpine.data('kaibutsu', () => ({
-        name: '',
+document.addEventListener("alpine:init", () => {
+  Alpine.data("kaibutsu", () => ({
+    name: "Gojira",
 
-        name_wa() {
-            const syllables = hepburn.splitRomaji(this.name);
-            const head = syllables.shift();
-            const tail = syllables.pop();
+    name_wa() {
+      const syllables = hepburn.splitRomaji(this.name);
+      const head = syllables.shift();
+      const tail = syllables.pop();
 
-            return [ heads[head], syllables.map((syl) => bodies[syl]), tails[tail] ];
-        },
+      return [heads[head], syllables.map((syl) => bodies[syl]), tails[tail]];
+    },
 
-        hiragana() {
-            return hepburn.toHiragana(this.name);
-        },
-        katakana() {
-            return hepburn.toKatakana(this.name);
-        },
-    }));
+    hiragana() {
+      return hepburn.toHiragana(this.name);
+    },
+    katakana() {
+      return hepburn.toKatakana(this.name);
+    },
+  }));
 });
 
 Alpine.start();
